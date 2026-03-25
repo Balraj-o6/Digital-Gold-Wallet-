2b7c9ebadf2ad2946303eeeffd5c3fdd7c88513c
Balraj-o6 <balrajkv666@gmail.com>
2026-03-25 17:53:00 +0530

    Added Entity virtualGoldHoldings

---
+++ Digital-Gold-Wallet/src/main/java/com/example/entity/VirtualGoldHoldings.java
@@ -0,0 +1,38 @@
+package com.example.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "virtual_gold_holdings")
+public class VirtualGoldHoldings {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer holdingId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "branch_id")
+    private VendorBranch branch;
+
+    private Double quantity;
+    private LocalDate createdAt;
+	
+	
+	
+}
