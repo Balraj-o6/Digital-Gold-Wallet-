67d967cf397b38f349eb89b728a3fe9ca87d4fe0
Anant <as9446947@gmail.com>
2026-03-25 17:49:23 +0530

    User entity created

---
+++ Digital-Gold-Wallet/src/main/java/entity/User.java
@@ -0,0 +1,47 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address
+
+    @Column(name = "balance", precision = 18, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    public User(){}
+
+    public User(String email, String name, Address address, BigDecimal balance, LocalDateTime createdAt) {
+        this.email = email;
+        this.name = name;
+        this.address = address;
+        this.balance = balance;
+        this.createdAt = createdAt;
+    }
+
+}
